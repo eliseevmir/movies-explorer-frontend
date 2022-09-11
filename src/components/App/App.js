02e7378e447893a716241fd52d4044f19e38a189
VLADIMIR BURLYAEV <eliseevmir@gmail.com>
2022-09-11 20:30:42 +0500

    create application react and added components

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+// import { Route, Switch, useHistory } from "react-router-dom";
+
+function App() {
+
+    return (
+        <>
+            <h1>DIPLOMA</h1>
+        </>
+    )
+};
+
+export default App;
